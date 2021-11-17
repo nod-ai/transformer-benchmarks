412c974cfa30918a4db17393eecd7fa8163fd743
Thomas Raoux <thomasraoux@google.com>
2021-11-16 18:51:13 -0800

    remove cpu flag for the gpu case

--- benchmark.py
+++ benchmark.py
@@ -297,7 +297,7 @@ def run_mlir(use_gpu, model_names, model_class, precision, num_threads, batch_si
     if use_gpu:
         backend = "cuda"
         backend_config = "cuda"
-        args = ["--iree-cuda-llvm-target-arch=sm_80", "--iree-llvm-target-cpu-features=host", "--iree-hal-cuda-disable-loop-nounroll-wa", "--iree-enable-fusion-with-reduction-ops"]
+        args = ["--iree-cuda-llvm-target-arch=sm_80", "--iree-hal-cuda-disable-loop-nounroll-wa", "--iree-enable-fusion-with-reduction-ops"]
 
     compiler_module = tfc.compile_module(BertModule(), exported_names = ["predict"], import_only=True)
     flatbuffer_blob = compile_str(compiler_module, target_backends=[backend], extra_args=args)
