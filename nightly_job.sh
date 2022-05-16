6ed8517ee0a4051d0a7219abd68439517776f55f
anush elangovan <anush@nod-labs.com>
2022-05-16 22:09:18 +0000

    update urls

--- nightly_job.sh
+++ nightly_job.sh
@@ -8,7 +8,7 @@ exec &> >(tee -a "$log_file")
 rm -rf $HOME/ci/nightly
 mkdir -p $HOME/ci/nightly
 cd $HOME/ci/nightly
-curl -O --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/powderluv/transformer-benchmarks/main/perf-ci.sh
+curl -O --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/nod-ai/transformer-benchmarks/main/perf-ci.sh
 chmod +x $HOME/ci/nightly/perf-ci.sh
 $HOME/ci/nightly/perf-ci.sh
 gsutil cp $log_file gs://iree-shared-files/nod-perf/logs/
