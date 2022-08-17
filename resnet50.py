c9cc725effa7b8070b69487c54dee5ba28cffa9b
Anush Elangovan <anush@nod-labs.com>
2022-08-17 05:32:59 +0000

    Add ONNX and PyTorch resnet50 models

--- resnet50.py
+++ resnet50.py
@@ -1,19 +1,42 @@
-from transformers import AutoFeatureExtractor
-from onnxruntime import InferenceSession
-from datasets import load_dataset
 import time
+import os
+from transformers import AutoFeatureExtractor, ResNetForImageClassification
+from datasets import load_dataset
 
-# load image
 dataset = load_dataset("huggingface/cats-image")
 image = dataset["test"]["image"][0]
 
-# load model
 feature_extractor = AutoFeatureExtractor.from_pretrained("microsoft/resnet-50")
-session = InferenceSession("model.onnx")
 
-# ONNX Runtime expects NumPy arrays as input
-inputs = feature_extractor(image, return_tensors="np")
-outputs = session.run(output_names=["last_hidden_state"], input_feed=dict(inputs))
+
+#PyTorch
+import torch
+
+model = ResNetForImageClassification.from_pretrained("microsoft/resnet-50")
+warmup = 5
+total_iter = 100
+num_iter = total_iter - warmup
+for i in range(num_iter):
+    if(i == warmup-1):
+        start = time.time()
+    inputs = feature_extractor(image, return_tensors="pt")
+    with torch.no_grad():
+        logits = model(**inputs).logits
+    predicted_label = logits.argmax(-1).item()
+end = time.time()
+total_time = end - start
+print("PyTorch: time/iter in ms : "+str(total_time*1000/num_iter))
+#print(model.config.id2label[predicted_label])
+
+
+# OnnxRuntime
+from onnxruntime import InferenceSession
+import urllib.request
+
+if not os.path.isfile("model.onnx"):
+    urllib.request.urlretrieve('https://huggingface.co/OWG/resnet-50/resolve/main/onnx/model.onnx',"model.onnx")
+
+session = InferenceSession("model.onnx")
 
 warmup = 5
 total_iter = 100
@@ -22,7 +45,10 @@ for i in range(num_iter):
     if(i == warmup-1):
         start = time.time()
     #print(BertCompiled.learn(predict_sample_input,np.random.randint(5, size=(BATCH_SIZE))))
+    # ONNX Runtime expects NumPy arrays as input
+    inputs = feature_extractor(image, return_tensors="np")
     outputs = session.run(output_names=["last_hidden_state"], input_feed=dict(inputs))
 end = time.time()
 total_time = end - start
-print("time/iter in ms : "+str(total_time*1000/num_iter))
+print("Onnx: time/iter in ms : "+str(total_time*1000/num_iter))
+
